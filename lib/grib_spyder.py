3e900d2a07510c3f368108d9a46f6be0aa253c5d
Matthew Pate <mcpate@gmail.com>
2013-07-01 21:47:15 -0700

    Updates init to take 'default or none' arguments. Adds method for downloading from full url. Adds class var = string to parameterized grib file.

--- lib/grib_spyder.py
+++ lib/grib_spyder.py
@@ -13,13 +13,15 @@ import os
 #() holds any inheritance
 class GribSpyder(object):
 
+    partial_grib = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_hd.pl?file=REPLACE&lev_500_mb=on&lev_750_mb=on&lev_800_mb=on&lev_1000_mb=on&all_var=on&leftlon=133&rightlon=95&toplat=55&bottomlat=25&dir=%2Fgfs.2013070200%2Fmaster'
+
     def __init__(self, args):
         self.link_parser = lib.html_parser.GRIBLinkParser()
-        self.url = args['url']
-	if 'store_loc' in args:
-		self.tmp_dir = args['store_loc']
-	else:
-        	self.tmp_dir = self.__build_tmp_path()
+        #optionally initialize url
+        self.url = self.__default_url(args)
+        # optionally initialize storage location
+        self.store_loc = self.__default_store_loc(args)
+
 
     # -opens url in try catch block. Will need to extend catches based on request type
     # -error object can hold server response. There is a dict of common responses (see bookmark)
@@ -46,6 +48,10 @@ class GribSpyder(object):
             print("link to download does not seem to exist.")
 
 
+    def download_file_by_url(self, url):
+        self.__download(url)
+
+
     # Private -------------------------------------------------------
 
     def __build_link(self, url, link):
@@ -54,20 +60,32 @@ class GribSpyder(object):
     # builds a path to 'tmp' which should be in project tree
     # NOTE: this expects tmp to exist one dir above location of this file, this
     # may need to be more flexible in the future...
-    def __build_tmp_path(self):
+    def __build_path_to_tmp(self):
         tmp_dir = os.path.join(os.path.dirname(__file__), os.pardir, 'tmp/')
         if os.path.exists(tmp_dir):
             print("found tmp for storage: " + tmp_dir)
             return tmp_dir
         else:
-            print("ERROR in GribSpyder.__build_tmp_path: tmp directory not found")
+            print("ERROR in GribSpyder.__build_path_to_tmp: tmp directory not found")
+            return None
+
+    def __default_store_loc(self, args):
+        if 'store_loc' in args:
+            return args['store_loc']
+        else:
+            return self.__build_path_to_tmp()
+
+    def __default_url(self, args):
+        if 'url' in args:
+            return args['url']
+        else:
             return None
 
     # 1. expects there to be a temp directory
     # 2. builds storage file path based on tmp path and base of url being downloaded
     # 3. attempts to download file
     def __download(self, url):
-        store_loc = self.tmp_dir + self.__get_url_base(url)
+        store_loc = self.store_loc + self.__get_url_base(url)
         print("attempting to download to tmp: " + url)
         urllib.request.urlretrieve(url, store_loc)
 
