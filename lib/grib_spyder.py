643e74d3e3543144bbfa8e0ecc84f2414fa11c42
Matthew Pate <mcpate@gmail.com>
2013-07-03 14:15:11 -0700

    Adds comments to grib_spyder source.

--- lib/grib_spyder.py
+++ lib/grib_spyder.py
@@ -14,9 +14,11 @@ import re
 #() holds any inheritance
 class GribSpyder(object):
 
+
     partial_grib_path = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_hd.pl?file=$FORECASTHOUR$&lev_500_mb=on&lev_750_mb=on&lev_800_mb=on&lev_1000_mb=on&all_var=on&leftlon=133&rightlon=95&toplat=55&bottomlat=25&dir=%2F$MODELRUN$%2Fmaster'
     forecast_hr_format = 'gfs.t$MRHOUR$z.mastergrb2f$FHOUR$'
     model_run_format = 'gfs.$MRDATE$'
+
 
     def __init__(self, args):
         self.link_parser = lib.html_parser.GRIBLinkParser()
@@ -25,8 +27,9 @@ class GribSpyder(object):
         # optionally initialize storage location
         self.store_loc = self.__default_store_loc(args)
 
+
     # -opens url in try catch block. Will need to extend catches based on request type
-    # -error object can hold server response. There is a dict of common responses (see bookmark)
+    # -error object can hold server response. There is a dict of common responses
     def get_html(self, url):
         try:
             response = urllib.request.urlopen(url)
@@ -37,6 +40,7 @@ class GribSpyder(object):
         except ValueError as err:
             return err
         return str(response.readall())
+
 
     # Gets html, makes sure link exists, attempts to download it.
     def download_file_by_link(self, link):
@@ -49,20 +53,34 @@ class GribSpyder(object):
         else:
             print("link to download does not seem to exist.")
 
+
+    # Straight up downloads the file.  No error checking.
     def download_file_by_url(self, url):
         self.__download(url)
 
-    #this should be passed the exact text that changes in the path
+
+    # Downloads file based on model_run and forecast_hr corresponding EXACTLY to the text that changes in the path.
+    # ex. download_param_grib('gfs.2013070100', 'gfs.t00z.mastergrb2f00')
     def download_param_grib(self, model_run, forecast_hr):
         path = self.__search_replace_partial_path(self.partial_grib_path, model_run, forecast_hr)
         self.__download(path)
 
+
+    # Downloads gribs as specified in a dictionary
+    # ex. download_param_gribs_dict({'gfs.2013070100' : ['gfs.t00z.mastergrb2f00', 'gfs.t00z.mastergrb2f03', etc.]})
     def download_param_gribs_dict(self, dict):
         for key in dict:
             for item in dict[key]:
                 print('attempting to download: ' + key + ' - ' + item)
                 self.download_param_grib(key, item)
 
+
+    # Downloads multiple grib files.
+    # model_run_dateHour:  integer describing dateHour (ex. 2013070100)
+    # fh_start:            integer describing the hour to start 00-180 (or greater)
+    # fh_end:              integer describing the hour to end
+    # increment:           integer describing how to increment the forecast hours being downloaded
+    # NOTE: the increment must correspond to an actual increment provided by NOAA
     def download_param_grib_range(self, model_run_dateHour, fh_start, fh_end, increment):
         start = int(fh_start)
         end = int(fh_end)
@@ -75,7 +93,6 @@ class GribSpyder(object):
               "............increment = " + str(inc))
         # Add in the part of the forecast path related to the model run: gfs.t$$z...
         forecast_hour = self.__add_to_forecast_hr('\$MRHOUR\$', self.forecast_hr_format, mr[-2:])
-        # Add in the part of the forecast path related to the forecast hour
         while start < end:
             # Add in the part of the forecast path related to the forecast hour
             one = self.__add_to_forecast_hr('\$FHOUR\$', forecast_hour, start)
@@ -91,6 +108,7 @@ class GribSpyder(object):
     def __build_link(self, url, link):
         return url + link
 
+
     # builds a path to 'tmp' which should be in project tree
     # NOTE: this expects tmp to exist one dir above location of this file, this
     # may need to be more flexible in the future...
@@ -102,6 +120,7 @@ class GribSpyder(object):
         else:
             print("ERROR in GribSpyder.__build_path_to_tmp: tmp directory not found")
             return None
+
 
     # expects '$HOUR$' to be in string, replaces it with 'hour'
     def __add_to_forecast_hr(self, regex, string, hour):
@@ -116,19 +135,10 @@ class GribSpyder(object):
                 hr = str(hour)
         return re.sub(regex, hr, string)
 
-    def __add_forecast_hr_forecast(self, string, hour):
-        hr = hour
-        #perform checks to avoid truncating leading 0
-        if type(hour) == int:
-            if hour == 0:
-                hr = '00'
-
-        string = str(string)
-        hour = str(hour)
-        return re.sub('\$FHOUR\$', hour, string)
 
     def __build_model_run(self, dateHour):
         return re.sub('\$MRDATE\$', str(dateHour), self.model_run_format)
+
 
     def __default_store_loc(self, args):
         if 'store_loc' in args:
@@ -136,11 +146,13 @@ class GribSpyder(object):
         else:
             return self.__build_path_to_tmp()
 
+
     def __default_url(self, args):
         if 'url' in args:
             return args['url']
         else:
             return None
+
 
     # 1. expects there to be a temp directory
     # 2. builds storage file path based on tmp path and base of url being downloaded
@@ -156,9 +168,11 @@ class GribSpyder(object):
     def __get_url_base(self, url):
         return url.rsplit('/', 1)[1]
 
+
     # verifies that a link (text) exists in the html
     def __link_exists_in_html(self, link, html):
         return self.link_parser.grib_link_exists(link, html)
+
 
     def __search_replace_partial_path(self, partial_path, model_run=None, forecast_hr=None):
         p = ''
@@ -166,7 +180,6 @@ class GribSpyder(object):
             p = re.sub('\$MODELRUN\$', model_run, partial_path)
         if forecast_hr:
             p = re.sub('\$FORECASTHOUR\$', forecast_hr, p)
-        print("modified 'partial' grib path: " + p)
         return p
 
 
