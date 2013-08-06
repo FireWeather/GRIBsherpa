d80d3e679d98b70c2ac11e1639640c2777422087
Matthew Pate <mcpate@gmail.com>
2013-08-06 08:30:46 -0700

    Refactors UrlParser functionality into seperate file within lib: url_parser.

--- lib/fetcher.py
+++ lib/fetcher.py
@@ -9,6 +9,7 @@ __author__ = 'MCP'
 import urllib.request
 import urllib.error
 import lib.html_parser
+import lib.url_parser
 import os
 import re
 
@@ -27,7 +28,7 @@ class Fetcher(object):
 
     ## @param args A dictionary of initialization params
     def __init__(self, args=None):
-        self.url_parser = UrlParser() #defined below
+        self.url_parser = lib.url_parser.UrlParser()
         self.html_parser = lib.html_parser.GribHtmlParser()
         self.url = self.__default_args("url", args)
         self.store_loc = self.__default_args("store_loc", args)
@@ -151,91 +152,3 @@ class Fetcher(object):
             return "0" + str(fh)
         else:
             return str(fh)
-
-
-
-class UrlParser(object):
-
-    ## The following class variables are used to specify download urls (and/or aspects of that url)
-    # ---- 0.5 degree hyperlink ----
-    # partial_gfs_path = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_hd.pl?file=$FORECASTHOUR$&lev_500_mb=on&lev_700_mb=on&lev_850_mb=on&lev_1000_mb=on&all_var=on&leftlon=133&rightlon=95&toplat=55&bottomlat=25&dir=%2F$MODELRUN$%2Fmaster'
-    # gfs_fh_format = 'gfs.t$MRHOUR$z.mastergrb2f$FHOUR$'
-
-    # ---- 1.0 degree hyperlink ----
-    # This is used for testing.
-    #partial_gfs_path = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs.pl?file=$FORECASTHOUR$&lev_850_mb=on&all_var=on&subregion=leftlon=133&rightlon=95&toplat=55&bottomlat=25&dir=%2F$MODELRUN$'
-
-    partial_gfs_path = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs.pl?file=$FORECASTHOUR$&lev_500_mb=on&lev_700_mb=on&lev_850_mb=on&lev_1000_mb=on&lev_surface=on&all_var=on&leftlon=-133&rightlon=-95&toplat=55&bottomlat=25&dir=%2F$MODELRUN$'
-    gfs_fh_format = 'gfs.t$MRHOUR$z.pgrbf$FHOUR$.grib2'
-    gfs_mr_format = 'gfs.$MRDATE$'
-
-    partial_nam_path = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_nam_na.pl?file=$FORECASTHOUR$&lev_500_mb=on&lev_700_mb=on&lev_850_mb=on&all_var=on&leftlon=133&rightlon=95&toplat=55&bottomlat=25&dir=%2F$MODELRUN$'
-    nam_fh_format = 'nam.t$MRHOUR$z.awip32$FHOUR$.tm00.grib2'
-    nam_mr_format = 'nam.$MRDATE$'
-
-    #lev_filters = { "all levels" : "all_levels=on",
-    #                "500 mb" : "lev_500_mb=on",
-    #                "700    mb" : "lev_700_mb=on",
-    #                "750 mb" : "lev_750_mb=on",
-    #                "800 mb" : "lev_800_mb=on",
-    #                "850 mb" : "lev_850_mb=on",
-    #                "1000 mb" : "lev_1000_mb=on" }
-                     
-
-    #var_filters = { "all variables" : "all_var=on" }
-
-
-    ## Uses mr_type and dateHour to build the corresponding model run
-    def build_model_run(self, mr_type, date_hour):
-        if mr_type.lower() == 'gfs':
-            return re.sub('\$MRDATE\$', str(date_hour), self.gfs_mr_format)
-        elif mr_type.lower() == 'nam':
-            return re.sub('\$MRDATE\$', str(date_hour), self.nam_mr_format)
-        else:
-            print('toots')
-
-    def build_download_url(self, model_type, mr_dateHour, forecast_hr):
-        model_run = self.build_model_run(model_type, mr_dateHour)
-        forecast_hour = self.__build_forecast_hr(model_type, mr_dateHour, forecast_hr)
-        if model_type.lower() == 'gfs':
-            return self.__search_replace_partial_path(self.partial_gfs_path, model_run, forecast_hour)
-        elif model_type.lower() == 'nam':
-            return self.__search_replace_partial_path(self.partial_nam_path, model_run, forecast_hour)
-        else:
-            print('euro model')
-
-    # -------------------------------- Private -------------------------------------
-    # expects '$HOUR$' to be in string, replaces it with 'hour'
-    def __add_to_forecast_hr(self, regex, string, hour):
-        hr = hour
-        #perform checks to avoid truncating leading 0
-        if type(hour) == int:
-            if hour == 0:
-                hr = '00'
-            elif hour < 10:
-                hr = '0' + str(hour)
-            else:
-                hr = str(hour)
-        return re.sub(regex, hr, string)
-
-    def __build_forecast_hr(self, mr_type, date_hour, forecast_hr):
-        mr = str(date_hour) #need str so index can happen below
-        if mr_type.lower() == 'gfs':
-            #add model run hour
-            tmp = self.__add_to_forecast_hr('\$MRHOUR\$', self.gfs_fh_format, mr[-2:])
-            #add forecast hour and return
-            return self.__add_to_forecast_hr('\$FHOUR\$', tmp, forecast_hr)
-        elif mr_type.lower() == 'nam':
-            #add model run hour todo: figure out where model run hour changes
-            tmp = self.__add_to_forecast_hr('\$MRHOUR\$', self.nam_fh_format, '00')
-            return self.__add_to_forecast_hr('\$FHOUR\$', tmp, forecast_hr)
-        else:
-            print('euro model')
-
-    def __search_replace_partial_path(self, partial_path, model_run=None, forecast_hr=None):
-        p = ''
-        if model_run:
-            p = re.sub('\$MODELRUN\$', model_run, partial_path)
-        if forecast_hr:
-            p = re.sub('\$FORECASTHOUR\$', forecast_hr, p)
-        return p
