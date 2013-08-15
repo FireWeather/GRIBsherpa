15f94125c25326aef3e16088b4dd9888c1f4a964
Matthew Pate <mcpate@gmail.com>
2013-08-15 13:46:12 -0700

    Addition to previous commit.  This creates log directory.

---
+++ lib/global.py
@@ -0,0 +1,32 @@
+# --------------------------------------------------------
+# Copyright (c) 2013 Matthew Pate and Daniel Catalano
+# [This program is licensed under the "MIT License"]
+# Please see the file COPYING in the source distribution
+# of this software for license terms.
+# --------------------------------------------------------
+
+import logging
+import os
+import date
+
+
+# ---------------------------- Logging global config ------------------------
+# In subsequent modules the logger will be used with the following call:
+# > global.logging.info()
+# > global.logging.warn()
+# > global.logging.debug()
+
+# This sets the initial logging functionality.  The first call through this sets the vars
+# the second + calls are essentially no ops as per the documentation.
+
+# verify log directory
+log_dir = os.path.join(os.path.dirname(__file__), os.pardir, 'log/')
+if not os.path.exists(log_dir):
+    print("Error in logger::createLogFile - " + log_dir + " does not exist.")
+
+# name logfile ...log/todaysdate
+logFile = log_dir + str(date.today())
+
+logging.basicConfig(filename=logFile, level=logging.INFO)
+
+
