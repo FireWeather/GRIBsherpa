2800e2e149131e99dc8a711d724a705b22a4ce04
Matthew Pate <mcpate@gmail.com>
2013-12-09 23:31:06 -0800

    Adds basic extraction functions to blender. Cleans up large amount of cruft.

--- lib/blender.py
+++ lib/blender.py
@@ -27,97 +27,34 @@ class Blender(object):
     # These will each have many key/value pairs. The message format matches exactly the grib format.
     # Note: for queries containing multiple heights, there will be several repeated (but different) messages for each height
     # MP: Not finding -- precipital water -- its listing below is a best guess at the Message name/format.
-    MOI = ['Best (4-layer) lifted index', 'Convective available potential energy', 'Geopotential Height', 'Precipital water', 'Surface lifted index', 'Temperature', 'Realative humidity', 'Surface pressure',  'U component of wind', 'V component of wind', 'Wind speed']
+    MOI = ['Best (4-layer) lifted index', 'Convective available potential energy', 'Geopotential Height', 'Precipital water', 'Surface lifted index', 'Temperature', 'Relative humidity', 'Surface pressure',  'U component of wind', 'V component of wind', 'Wind speed']
 
 
     ## "Fields of Interest" These are the fields contained within the messages (above) that we want to capture.
     # Note that some of these fields (latitudes for example) will contain multiple values.
     FOI = ['name', 'level', 'values', 'units', 'latitudes', 'longitudes', 'distinctLongitudes', 'distinctLatitudes']
 
+    ## Takes a path to the grib file to work with.  Tries to open it for subsequent work.
+    def __init__(self, pathToGrib):
+        self.grib = self.__openGrib(pathToGrib)
 
-    # TODO: clean up comments
-    ## This is the work horse of the blender class.  It searches the grib for matching messages and returns a <??>
-    #  of all data found for the matching fields.  If the msgs and fields params are left as None, the above
-    #  hardcoded values will be used (MOI, FOI)
-    #  @param msgs      The messages to look for. Type: Array
-    #  @param fields    The fields to return for each message. Type: Array
-    #  @param grib      The grib file to use. Type: grib
-    #  @return          Returns a <type> of all data found
-    #
-    # { "Geopotential Height" : [
-    #                             { level : 500, units : "mbar", "120, 95" : 243, "120, 94" : 243, "120, 93" : 243, ... },
-    #                             { level : 550, units : "mbar", "120, 95" : 243, "120, 94" : 243, "120, 93" : 243, ... },
-    #                           ],
-    #   "Precipital Water"    : [
-    #                             { level : ..... }
-    #                           ]
-    # }
-    #
-    def getSpecificData(self, gribPath, latLons, msgs=None, fields=None):
-        toReturn = {}
-        gribMsgsFound = []
-        fp = self.__openGrib(gribPath)
+    ## Returns the date and time of the model run.
+    def modelRun(self):
+        return self.grib.dataDate, self.grib.dataTime
 
-        # check if nothing passed in and conditionally set params
-        if msgs is None:
-            msgs = self.MOI
-        if fields is None:
-            fields = self.FOI
+    ## Returns the forecast hour.
+    def forecastHour(self):
+        return self.grib.forecastTime
 
-        # Find all matching messages (safety check to ensure messages we're trying to find actually exist)
-        # ValueError appended here if message isn't found in __getMessage function.
-        # This returns a 2d "list of lists"
-        for str in msgs:
-            gribMsgsFound.append( self.__getMessage(str, fp) )
+    ## Returns in order: attribute, attribute_unit, level, level_unit
+    # TODO: figure out if pressureUnits is right param
+    def metParams(self):
+        return self.grib.name, self.grib.units, self.grib.level, self.grib.pressureUnits
 
-        # For each array of matching grib messages (1..*)
-        for gribMsgs in gribMsgsFound:
-            if type(gribMsgs) != ValueError:
-                # use the first name of 1..* in toReturn[name] as all names should all be the same
-                toReturn[gribMsgs[0].name] = self.__parseGribMsg(gribMsgs, latLons)
+    def values(self):
+        # Using Numpy ndarray method
+        return self.grib.values.flatten
 
-        # At this point, all data for each message type should be organized in toReturn
-        return toReturn
-
-
-
-    ## Returns a list of all matching messages found. Msgs can either be a
-    #  single message type (ex. "Temperature") or it can be a list of messages
-    #  (ex. ["Temperature", "Geopotential Height",...])
-    #  @param msgs    single item (string) or array
-    #  @return        returns an array of matches
-    def getMessages(self, msgs, grib):
-        fp = self.__openGrib(grib)
-        toReturn = []
-        if type(msgs) == list:
-            for msg in msgs:
-                matches = self.__getMessage(msg, grib)
-                for match in matches:
-                    toReturn.append(match)
-            return toReturn
-        else:
-            return self.__getMessage(msgs, grib)
-
-
-    # todo: depreciate this
-    ## Breaks down msg (pygrib mesage) into a dictionary. Will return all fields
-    # if list is not specified else will try to return fields specified in list.
-    # @param msg pygrib message
-    # @param list fields you want data for
-    # @return Dictionary of data for message
-    def getMessageFields(self, msg, list=None):
-        dict = {}
-        if list is None:
-            for key in msg.keys():
-                val = msg[key]
-                dict[key] = val
-        else:
-            for field in list:
-                if field in msg.keys():
-                    dict[field] = msg[field]
-                else:
-                    dict[field] = "NOT FOUND"
-        return dict
 
 
     ## This returns data for a specific point in a grid.  If data is requested for a point that doesn't exist
@@ -218,25 +155,6 @@ class Blender(object):
             log.write.error("Error in blender::__openGrib - Grib file not found")
             raise err
         return f
-
-    ## Parses data out of grib messages.
-    #  @return  Array of dictionaries, 1 for each gribMsg in gribMsgs corresponding to level
-    def __parseGribMsg(self, gribMsgs, latLons):
-        toReturn = []
-        levelData = {}
-
-        for msg in gribMsgs:
-            levelData["level"] = msg.level
-            levelData["units"] = msg.units
-
-            # TODO: figure out how to append to dataFound ' latLon : value ' Not sure what latLon is. Eventually this step will involve interpolated data
-            #for loc in latLons:
-            #    dataFound[loc] = gribMsg["values"][##somepoint##]
-
-            toReturn.append(levelData)
-            levelData = {}
-
-        return toReturn
 
 
 
