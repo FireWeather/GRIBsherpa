80cbeadaab06e0b7abd80eee0419120c37ba36e2
Matthew Pate <mcpate@gmail.com>
2013-12-20 18:37:34 -0800

    Ran through database schema and finalized all table creation(s), verified unit/types,verified keys, verified naming, etc. Split up met_params table into level_params and attribute_params.

--- .vmconfig/dbstormking_create_schema.sql
+++ .vmconfig/dbstormking_create_schema.sql
@@ -28,36 +28,50 @@ CREATE TABLE model_forecast_relation (
   UNIQUE (model_run_pkey, forecast_hour_pkey)
 );
 
+CREATE TABLE level_param (
+  level smallint NOT NULL UNIQUE,
+  level_unit text NOT NULL,
+  level_web text NOT NULL UNIQUE,
+  level_param_pkey SERIAL PRIMARY KEY
+);
+
+CREATE TABLE attribute_param (
+  attribute text NOT NULL UNIQUE,
+  attribute_unit text NOT NULL,
+  attribute_web text NOT NULL UNIQUE,
+  attribute_param_pkey SERIAL PRIMARY KEY
+);
+
 CREATE TABLE met_param (
-  level smallint NOT NULL, 
-  level_unit text NOT NULL, 
-  attribute text NOT NULL, 
-  attribute_unit text NOT NULL, 
+  level_param_pkey integer NOT NULL REFERENCES level_param (level_param_pkey),
+  attribute_param_pkey integer NOT NULL REFERENCES attribute_param (attribute_param_pkey),
   met_param_pkey SERIAL PRIMARY KEY,
-  UNIQUE (level, attribute)
+  UNIQUE (level_param_pkey, attribute_param_pkey)
 );
 
 CREATE TABLE region (
-  region_name text NOT NULL,
-  region_abbv text UNIQUE NOT NULL,
+  gacc_name text NOT NULL UNIQUE,
+  gacc_abbv text NOT NULL UNIQUE,
   fs_region_num smallint NOT NULL,
   region_pkey SERIAL PRIMARY KEY
 );
 
--- TODO create uniqueness in region_ref_number & regional_grid
 CREATE TABLE model_grid_points (
-  model_grid_points_pkey SERIAL PRIMARY KEY, 
-  region_pkey smallint NOT NULL REFERENCES region (region_pkey),
+  region_pkey integer NOT NULL REFERENCES region (region_pkey),
   region_ref_number smallint NOT NULL, 
   regional_grid text,
-  national_ref_number smallint UNIQUE, 
-  location bigint -- TODO replace "bigint" with "geography(POINT,4326)"
+  national_ref_number smallint UNIQUE,
+  location geography(POINT) NOT NULL,
+  model_grid_points_pkey SERIAL PRIMARY KEY,
+  UNIQUE(region_pkey, region_ref_number, regional_grid)
+  --location geography(POINT, 4326) NOT NULL
 );
 
 CREATE TABLE met_data ( 
-    value real NOT NULL,
-    met_data_pkey SERIAL PRIMARY KEY, 
-    met_param_pkey smallint NOT NULL REFERENCES met_param (met_param_pkey),
-    model_grid_points_pkey integer NOT NULL REFERENCES model_grid_points (model_grid_points_pkey), 
-    model_forecast_relation_pkey integer NOT NULL REFERENCES model_forecast_relation (model_forecast_relation_pkey) 
+  value real NOT NULL,
+  met_param_pkey integer NOT NULL REFERENCES met_param (met_param_pkey),
+  model_grid_points_pkey integer NOT NULL REFERENCES model_grid_points (model_grid_points_pkey),
+  model_forecast_relation_pkey integer NOT NULL REFERENCES model_forecast_relation (model_forecast_relation_pkey),
+  met_data_pkey SERIAL PRIMARY KEY,
+  UNIQUE(met_param_pkey, model_grid_points_pkey, model_forecast_relation_pkey)
 );
