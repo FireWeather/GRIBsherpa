f1cf41356ba739a8a7002a23c45f1d8441f19b12
Dan Catalano <daniel.w.catalano@gmail.com>
2014-01-12 15:52:11 -0800

    Finesse ... creates all tables under the correct schema and the extension is created under that schema as well

--- .vmconfig/dbstormking_create_schema.sql
+++ .vmconfig/dbstormking_create_schema.sql
@@ -6,59 +6,60 @@
 -- --------------------------------------------------------
 
 \c stormking
+CREATE SCHEMA development;
+SET search_path TO development;
 CREATE EXTENSION postgis;
-CREATE SCHEMA production;
 
-CREATE TABLE production.model_run (
+CREATE TABLE model_run (
   model_run_date date NOT NULL,
   model_run_time timetz NOT NULL,
   model_run_pkey SERIAL PRIMARY KEY,
   UNIQUE (model_run_date, model_run_time)
 );
 
-CREATE TABLE production.forecast_hour (
+CREATE TABLE forecast_hour (
   forecast_hour interval NOT NULL,
   forecast_hour_pkey SERIAL PRIMARY KEY,
   UNIQUE (forecast_hour)
 );
 
-CREATE TABLE production.model_forecast_relation (
-  model_run_pkey integer NOT NULL REFERENCES production.model_run (model_run_pkey),
-  forecast_hour_pkey integer NOT NULL REFERENCES production.forecast_hour (forecast_hour_pkey),
+CREATE TABLE model_forecast_relation (
+  model_run_pkey integer NOT NULL REFERENCES model_run (model_run_pkey),
+  forecast_hour_pkey integer NOT NULL REFERENCES forecast_hour (forecast_hour_pkey),
   model_forecast_relation_pkey SERIAL PRIMARY KEY,
   UNIQUE (model_run_pkey, forecast_hour_pkey)
 );
 
-CREATE TABLE production.level_param (
+CREATE TABLE level_param (
   level text NOT NULL UNIQUE,
   level_unit text NOT NULL,
   level_web text NOT NULL UNIQUE,
   level_param_pkey SERIAL PRIMARY KEY
 );
 
-CREATE TABLE production.attribute_param (
+CREATE TABLE attribute_param (
   attribute text NOT NULL UNIQUE,
   attribute_unit text NOT NULL,
   attribute_web text NOT NULL UNIQUE,
   attribute_param_pkey SERIAL PRIMARY KEY
 );
 
-CREATE TABLE production.met_param (
-  level_param_pkey integer NOT NULL REFERENCES production.level_param (level_param_pkey),
-  attribute_param_pkey integer NOT NULL REFERENCES production.attribute_param (attribute_param_pkey),
+CREATE TABLE met_param (
+  level_param_pkey integer NOT NULL REFERENCES level_param (level_param_pkey),
+  attribute_param_pkey integer NOT NULL REFERENCES attribute_param (attribute_param_pkey),
   met_param_pkey SERIAL PRIMARY KEY,
   UNIQUE (level_param_pkey, attribute_param_pkey)
 );
 
-CREATE TABLE production.region (
+CREATE TABLE region (
   gacc_name text NOT NULL UNIQUE,
   gacc_abbv text NOT NULL UNIQUE,
   fs_region_num smallint NOT NULL,
   region_pkey SERIAL PRIMARY KEY
 );
 
-CREATE TABLE production.model_grid_points (
-  region_pkey integer NOT NULL REFERENCES production.region (region_pkey),
+CREATE TABLE model_grid_points (
+  region_pkey integer NOT NULL REFERENCES region (region_pkey),
   region_ref_number smallint NOT NULL, 
   regional_grid text,
   national_ref_number smallint UNIQUE,
@@ -67,11 +68,11 @@ CREATE TABLE production.model_grid_points (
   UNIQUE(region_pkey, region_ref_number, regional_grid)
 );
 
-CREATE TABLE production.met_data ( 
+CREATE TABLE met_data (
   value real NOT NULL,
-  met_param_pkey integer NOT NULL REFERENCES production.met_param (met_param_pkey),
-  model_grid_points_pkey integer NOT NULL REFERENCES production.model_grid_points (model_grid_points_pkey),
-  model_forecast_relation_pkey integer NOT NULL REFERENCES production.model_forecast_relation (model_forecast_relation_pkey),
+  met_param_pkey integer NOT NULL REFERENCES met_param (met_param_pkey),
+  model_grid_points_pkey integer NOT NULL REFERENCES model_grid_points (model_grid_points_pkey),
+  model_forecast_relation_pkey integer NOT NULL REFERENCES model_forecast_relation (model_forecast_relation_pkey),
   met_data_pkey SERIAL PRIMARY KEY,
   UNIQUE(met_param_pkey, model_grid_points_pkey, model_forecast_relation_pkey)
 );
